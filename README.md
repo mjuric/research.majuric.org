c26b865e2e09c4c9f48a34c101f5337e28606e42
Mario Juric <mjuric@astro.washington.edu>
2016-12-31 23:16:59 -0800

    First live version of research.majuric.org
    ==========
    
    Started building sites ...
    Built site for language en:
    0 of 2 drafts rendered
    0 future content
    0 expired content
    10 regular pages created
    13 other pages created
    0 non-page files copied
    9 paginator pages created
    8 tags created
    0 categories created
    total in 69 ms

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This website is to be built with Hugo.
 
 While editing locally, run hugo as:
 ```
-hugo server --bind 0.0.0.0 -b http://gallifrey.local:1313/ --watch
+hugo server -b http://localhost:1313/ --watch
 ```
 
 Use `./build.sh` to for final build, commit, and deployment.
--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 trap "{ rm -f commit.template; }" EXIT
 
 # Build the website
-hugo -b https://mjuric.github.io/research.majuric.org/ -d docs 2>&1 | tee commit.template
+hugo -d docs 2>&1 | tee commit.template
 
 # Replace the docs/ directory with the newly generated website
 git add -A docs
